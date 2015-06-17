644e90c1aeb9da2f601252dbf8e1179f22c1066f
Patrick B Warren <patrickbwarren@gmail.com>
2015-06-17 23:20:12 +0100

    more progress

--- svx_output.py
+++ svx_output.py
@@ -7,6 +7,7 @@ Python script for exporting survex (.svx) file from Inkscape
 Copyright (C) 2015 Patrick B Warren
 
 Email: patrickbwarren@gmail.com
+
 Paper mail: Dr Patrick B Warren, 11 Bryony Way, Birkenhead,
   Merseyside, CH42 4LY, UK.
 
@@ -35,21 +36,12 @@ import simplestyle
 
 def sprintd(b):
     "Takes a bearing and returns it as string in 000 format"
-    while (b < 0):
+    while b < 0:
         b += 360
     b = int(b + 0.5)
-    while (b >= 360):
+    while b >= 360:
         b -= 360
     return "%03i" % b
-
-station_x = []
-station_y = []
-station_id = []
-
-def push_station(station, x, y):
-    station_x.append(x)
-    station_y.append(y)
-    station_id.append(station)
 
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
@@ -108,8 +100,6 @@ docname = svg.attrib['{%s}docname' % inkex.NSS[u'sodipodi']]
 print 'docname =', docname
 
 # el = svg.find('.//svg:image', namespaces=inkex.NSS)
-# el = svg.find('.//{http://www.w3.org/2000/svg}image')
-
 el = svg.find('.//{%(svg)s}image' % {'svg':inkex.NSS[u'svg']})
 absref = el.attrib['{%s}absref' % inkex.NSS[u'sodipodi']]
 path, img = os.path.split(absref)
@@ -124,11 +114,9 @@ path_stroke = []
 path_layer = []
   
 # list = svg.findall('.//svg:g/svg:path', namespaces=inkex.NSS)
-# list = svg.findall('.//{http://www.w3.org/2000/svg}g/{http://www.w3.org/2000/svg}path')
+pathlist = svg.findall('.//{%(svg)s}g/{%(svg)s}path' % {'svg':inkex.NSS[u'svg']})
 
-list = svg.findall('.//{%(svg)s}g/{%(svg)s}path' % {'svg':inkex.NSS[u'svg']})
-
-for path in list:
+for path in pathlist:
     path_id.append(path.attrib['id'])
     path_d.append(path.attrib['d'])
     path_stroke.append(simplestyle.parseStyle(path.attrib['style'])['stroke'])
@@ -159,13 +147,10 @@ if not found:
 # (ex, ey) to point along E.  We correct for north later.
 
 steps = simplepath.parsePath(path_d[i])
-dx = steps[1][1][0] - steps[0][1][0]
-dy = steps[1][1][1] - steps[0][1][1]
+dx, dy = steps[1][1][0]-steps[0][1][0], steps[1][1][1]-steps[0][1][1]
 dl = math.sqrt(dx*dx + dy*dy)
-nx = dx / dl
-ny = dy / dl
-ex = - ny
-ey = nx
+nx, ny = dx/dl, dy/dl
+ex, ey = -ny, nx
 
 # Figure out the scale vector (blue is 0000ff) and calculate scale factor
 
@@ -181,41 +166,98 @@ if not found:
     sys.exit(1)
 
 steps = simplepath.parsePath(path_d[i])
-dx = steps[1][1][0] - steps[0][1][0]
-dy = steps[1][1][1] - steps[0][1][1]
+dx, dy = steps[1][1][0]-steps[0][1][0], steps[1][1][1]-steps[0][1][1]
 dl = math.sqrt(dx*dx + dy*dy)
-sf = e.options.scale / dl
+scalefac = e.options.scale / dl
 
-# Now build the survex traverses.  The survex legs are collected as a
-# multiline string.  Also keep track of stations and absolute (in SVG coords)
-# positions to identify equates and exports.
+print 'scalefac = ', scalefac
+
+# Now build the survex traverses.  Also keep track of stations and
+# absolute (in SVG coords) positions to identify equates and exports.
 
 traverse_id = []
 traverse_legs = []
 
+station_x = []
+station_y = []
+station_id = []
+
 for i in range(len(path_id)):
-    if (path_stroke[i] == "#ff0000" and (e.options.layer == "" or path_layer[i] == e.options.layer)):
+    if path_stroke[i] == "#ff0000" and (e.options.layer == "" or path_layer[i] == e.options.layer):
         traverse = path_id[i]
         traverse_id.append(traverse)
         steps = simplepath.parsePath(path_d[i])
-        push_station("%s.0" % traverse, steps[0][1][0], steps[0][1][1])
+        for j in range(len(steps)):
+            station_x.append(steps[j][1][0])
+            station_y.append(steps[j][1][1])
+            station_id.append(traverse + '.%i' % j)
         legs = []
         for j in range(1, len(steps)):
-            push_station("%s.%i" % (traverse, j), steps[j][1][0], steps[j][1][1])
-            dx = steps[j][1][0] - steps[j-1][1][0]
-            dy = steps[j][1][1] - steps[j-1][1][1]
+            dx, dy = steps[j][1][0]-steps[j-1][1][0], steps[j][1][1]-steps[j-1][1][1]
             dl = math.sqrt(dx*dx + dy*dy)
-            tape = sf * dl
+            tape = scalefac * dl
             compass = e.options.north + math.degrees(math.atan2(ex*dx+ey*dy, nx*dx+ny*dy))
             legs.append("%3i %3i %7.2f  " % (j-1, j, tape) + sprintd(compass) + "  0.0")
-            
         traverse_legs.append(legs)
 
-for i in range(len(traverse_id)):
-    print "*begin", traverse_id[i]
-    for j in range(len(traverse_legs[i])):
-        print traverse_legs[i][j]
-    print "*end", traverse_id[i]
+ntraverse = len(traverse_id)
+nstation = len(station_id)
 
-for i in range(len(station_id)):
-    print station_id[i], "(%g, %g)" % (station_x[i], station_y[i])    
+# Identify the equates.  This is an O(n^2) pairwise comparison and
+# more efficient methods are available but n should not get too large:
+# for a large project it almost always bound to be a good idea to
+# break the survey up into manageable chunks, each of which can be
+# allocated its own survex file.  This can be facilitated by putting
+# different sections into different inkscape layers.
+
+equates = []
+
+if nstation > 1:
+    for i in range(nstation-1):
+        for j in range(i+1, nstation):
+            dx, dy = station_x[i]-station_x[j], station_y[i]-station_y[j]
+            dl = scalefac * math.sqrt(dx*dx + dy*dy)
+            if dl < e.options.tol:
+                equates.append('*equate %s %s ; dl = %4.2f m' % (station_id[i], station_id[j], dl))
+
+# Afficianados will notice this is a job only half done.  What we have
+# generated is an (incomplete) list of equivalence relations between
+# stations.  It may be incomplete because if A is near B, and B is
+# near C, it doesn't always follow that A is near enough C to satisfy
+# the closeness criterion.  What we should really do next is build the
+# set of equivalence classes of stations, then we can generate
+# precisely n-1 *equate directives for each non trivial equivalence
+# class of size n > 1.  In fact, survex allows for mutiple stations to
+# be listed in one *equate line, so we could just generate one *equate
+# directive for each non trivial equivalence class.  However survex
+# doesn't complain if there is redundant information in the equate
+# directives so below we take the easy option of using the list of
+# equivalence relations to generate a 1:1 list of equate directives.
+# Fastidiuous people may wish to tidy this up by hand afterwards.
+
+# Extract the set of stations required for export from the list of
+# equates.
+
+exportset = set()
+
+for equate in equates:
+    station1, station2 = equate.split(' ', 4)[1:3]
+    exportset.add(station1)
+    exportset.add(station2)
+
+# We use a dictionary to keep track of stations which should be
+# exported from a given traverse.
+
+exportdict = dict()
+
+for export in exportset:
+    traverse, station = export.split('.', 2)
+    if traverse in exportdict:
+        exportdict[traverse].append(station)
+    else:
+        exportdict[traverse] = [station]
+
+for k, v in exportdict.iteritems():
+    print k, "-->", ' '.join(sorted(v))
+
+
