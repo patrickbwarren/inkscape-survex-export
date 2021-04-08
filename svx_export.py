1eef3dd4a2dc69920ad6106f1166ec168f2ea51f
Patrick B Warren <patrickbwarren@gmail.com>
2021-04-08 20:34:35 +0100

    another update

--- svx_export.py
+++ svx_export.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
-"""
-svx_output.py
-Python script for exporting survex (.svx) file from Inkscape plan
+"""svx_output.py
+Python script for exporting survex (.svx) files from Inkscape
+
+The same script is used for both plan and elevation exports since 90%
+of the code is common.  The boolean command line option 'plan' selects
+between them.  In the .inx files, this option is set true or false by
+a gui-hidden parameter.
 
 Copyright (C) 2015, 2020, 2021 Patrick B Warren
 
@@ -20,6 +24,7 @@ General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see
 <http://www.gnu.org/licenses/>.
+
 """
 
 import os
@@ -27,7 +32,7 @@ import sys
 import inkex
 from time import strftime
 from itertools import combinations
-from math import sqrt, atan2, degrees
+from math import sqrt, atan2, degrees, copysign
 
 class PathError(Exception):
     pass
@@ -96,11 +101,12 @@ class ExportSurvex(inkex.EffectExtension):
         pars.add_argument('--overwrite', type=inkex.Boolean, help="Overwrite .svx file if it already exists")
         pars.add_argument('--splay', type=inkex.Boolean, help="Interpret dashed lines as splays")
         pars.add_argument('--scale', type=float, default=100.0, help="Length of scale bar (in m)")
-        pars.add_argument('--north', type=float, default=0.0, help="Bearing for orientation line (in degrees)")
+        pars.add_argument('--north', type=float, default=0.0, help="Bearing for orientation (in degrees)")
         pars.add_argument('--tol', type=float, default=0.2, help="Tolerance to equate stations (in m)")
         pars.add_argument('--path-color', type=inkex.Color, default=inkex.Color("red"), help="Path export color")
         pars.add_argument('--scale-color', type=inkex.Color, default=inkex.Color("blue"), help="Path export color")
         pars.add_argument('--orient-color', type=inkex.Color, default=inkex.Color("green"), help="Path export color")
+        pars.add_argument('--plan', type=inkex.Boolean, help="export as plan, else elevation")
         pars.add_argument('--debug', type=inkex.Boolean, help="dump information")
         
     def effect(self):
@@ -174,12 +180,13 @@ class ExportSurvex(inkex.EffectExtension):
         # point along E.  These are notional directions, and the real
         # orientations are used in the calculations below.
 
-        try:
-            dl, dx, dy = self.delta(*self.extract_line(self.options.orient_color))
-            nx, ny = -dx/dl, -dy/dl
-            ex, ey = -ny, nx
-        except PathError as err:
-            raise inkex.AbortExtension(f'Orientation vector: {err}')
+        if self.options.plan:
+            try:
+                dl, dx, dy = self.delta(*self.extract_line(self.options.orient_color))
+                nx, ny = -dx/dl, -dy/dl
+                ex, ey = -ny, nx
+            except PathError as err:
+                raise inkex.AbortExtension(f'Orientation vector: {err}')
 
         # Find the exportable (poly)lines, restricted to selected layer if desired
         
@@ -224,8 +231,13 @@ class ExportSurvex(inkex.EffectExtension):
                 if i:
                     dl, dx, dy = self.delta(pos, prev)
                     tape = scale_fac * dl
-                    compass = self.options.north + degrees(atan2(ex*dx+ey*dy, nx*dx+ny*dy))
-                    legs.append({'from': str(i-1), 'to': str(i), 'tape': tape, 'compass': compass})
+                    if self.options.plan:
+                        depth_change = 0.0
+                        compass = self.options.north + degrees(atan2(ex*dx+ey*dy, nx*dx+ny*dy))
+                    else: # elevation
+                        depth_change = scale_fac * dz
+                        compass = self.options.north - copysign(90.0, dx)
+                    legs.append({'from': str(i-1), 'to': str(i), 'tape': tape, 'compass': compass, 'depth_change': depth_change})
                 prev = pos
             traverses.append({'id': line['id'], 'legs': legs, 'splay': line['dashed']})
 
@@ -285,8 +297,11 @@ class ExportSurvex(inkex.EffectExtension):
                 f.write(f"; embedded image file name {img_file}\n")
             f.write(f"; generated {strftime('%c')}\n\n")
 
-            f.write(f"; SVG orientation: vector ({nx:0.3f}, {ny:0.3f}) is {self.round360(self.options.north):0.1f} degrees\n")
-            f.write(f"; SVG orientation: vector ({ex:0.3f}, {ey:0.3f}) is {self.round360(self.options.north+90):0.1f} degrees\n")
+            if self.options.plan:
+                f.write(f"; SVG orientation: vector ({nx:0.3f}, {ny:0.3f}) is {self.round360(self.options.north):0.1f} degrees\n")
+                f.write(f"; SVG orientation: vector ({ex:0.3f}, {ey:0.3f}) is {self.round360(self.options.north+90):0.1f} degrees\n")
+            else:
+                f.write(f"; SVG orientation: facing direction is {self.round360(self.options.facing):0.1f} degrees\n")
             f.write(f"; SVG scale: {scale_len:0.2f} units = {self.options.scale:0.1f} m, scale factor {scale_fac:0.4f}\n")
             f.write(f"; SVG contained {ntraverse} traverses and {nstation} stations\n")
             f.write(f"; SVG tolerance for identifying equates {self.options.tol:0.2f} m\n")
@@ -299,7 +314,10 @@ class ExportSurvex(inkex.EffectExtension):
                     stations = [f"{station['traverse']}.{station['id']}" for station in el['pair']]
                     f.write(f"*equate {stations[0]} {stations[1]} ; separation {el['sepn']:0.3f} m\n")
 
-            f.write("\n*data normal from to tape compass clino\n")
+            if self.options.plan:
+                f.write("\n*data normal from to tape compass clino\n")
+            else:
+                f.write("\n*data diving from to tape compass depthchange\n")
 
             for traverse in traverses:
                 if len(traverses) > 1:
@@ -310,7 +328,7 @@ class ExportSurvex(inkex.EffectExtension):
                 with splayFlags(self.options.splay and traverse['splay'], f):
                     for leg in traverse['legs']:
                         f.write(f"{leg['from']:3s} {leg['to']:3s} {leg['tape']:8.3f} ")
-                        f.write(f"{self.round360(leg['compass']):6.1f} 0.0\n")
+                        f.write(f"{self.round360(leg['compass']):6.1f} {leg['depth_change']:8.3f}\n")
                 if len(traverses) > 1:
                     f.write(f"\n*end {traverse['id']}\n")
 
